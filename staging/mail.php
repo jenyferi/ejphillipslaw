4680b177b35110f9ca81f93c6d720282e141777c
jenyferi <jenyferi@gmail.com>
2012-11-12 18:55:56 -0800

    completed IE fixes, working on contact form

--- staging/mail.php
+++ staging/mail.php
@@ -2,11 +2,15 @@
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
+$formcontent="$message";
 $recipient = "jenyferi@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+$subject = "The Law Office of Eric J. Phillips Website Contact Form";
+$mailheader = "MIME-Version: 1.0\r\n"
+  ."Content-Type: text/plain; charset=utf-8\r\n"
+  ."Content-Transfer-Encoding: 8bit\r\n"
+  ."From: =?UTF-8?B?". base64_encode($name) ."?= <$email>\r\n"
+  ."X-Mailer: PHP/". phpversion();
+
+mail($recipient, $subject, $formcontent, $mailheader);
 ?>
  

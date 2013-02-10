c81c1843b2793997f991b8fe6262c8f2f594afb2
Jenny Lin <jenyferi@gmail.com>
2013-02-10 15:17:22 -0800

    :(

--- staging/mail.php
+++ staging/mail.php
@@ -4,14 +4,14 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 $sender = "ejphillips@ehost-services222.com";
 $formcontent="$message";
-$recipient = "ephillips@ejphillipslaw.com";
+$recipient = "jennymhlin@gmail.com";
 $subject = "The Law Office of Eric J. Phillips Website Contact Form";
 $mailheader = "MIME-Version: 1.0\r\n"
-  ."Content-Type: text/plain; charset=utf-8\r\n"
-  ."Content-Transfer-Encoding: 8bit\r\n"
-  ."From: =?UTF-8?B?". base64_encode($name) ."?= <$sender>\r\n"
-  ."Reply-To: =?UTF-8?B?". base64_encode($name) ."?= <$email>\r\n"
-  ."X-Mailer: PHP/". phpversion();
+."Content-Type: text/plain; charset=utf-8\r\n"
+."Content-Transfer-Encoding: 8bit\r\n"
+."From: =?UTF-8?B?". base64_encode($name) ."?= <$sender>\r\n"
+."Reply-To: =?UTF-8?B?". base64_encode($name) ."?= <$email>\r\n"
+."X-Mailer: PHP/". phpversion();
 
 $messagesent = mail($recipient, $subject, $formcontent, $mailheader);
 header("HTTP/1.1 303 See Other");
@@ -23,4 +23,3 @@ if($messagesent)
 	header("Location: contact.html?sent=false");
 }
 ?>
- 
